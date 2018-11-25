c382a10d3e5ce7e86c748dd2f378c0b4e9250209
takos78 <akos.tomasits@gmail.com>
2018-11-25 17:12:55 +0100

    #1 Initial commit created

---
+++ src/main/java/my/spring5/recepie/Spring5RecepieAppApplication.java
@@ -0,0 +1,12 @@
+package my.spring5.recepie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecepieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecepieAppApplication.class, args);
+	}
+}
---
+++ src/test/java/my/spring5/recepie/Spring5RecepieAppApplicationTests.java
@@ -0,0 +1,16 @@
+package my.spring5.recepie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5RecepieAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
