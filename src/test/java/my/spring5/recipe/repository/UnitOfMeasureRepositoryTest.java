538749e377ef610053fe3458b2c4d65fa12acea2
takos78 <akos.tomasits@gmail.com>
2019-03-14 11:55:18 +0100

    Spring JPA UOM integration test added.

---
+++ src/test/java/my/spring5/recipe/repository/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,34 @@
+package my.spring5.recipe.repository;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import my.spring5.recipe.domain.UnitOfMeasure;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTest {
+	
+	@Autowired
+	public UnitOfMeasureRepository repository;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testFindByDescription() {
+		Optional<UnitOfMeasure> uom = repository.findByDescription("Teaspoon");
+		
+		assertEquals("Teaspoon", uom.get().getDescription());
+	}
+
+}
